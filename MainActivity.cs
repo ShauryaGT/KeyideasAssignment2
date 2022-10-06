64fb95f58e6c56a84ca2183471ba5a8fff87eab8
ShauryaGT <113298338+ShauryaGT@users.noreply.github.com>
2022-10-06 09:32:03 +0530

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,100 @@
+﻿using Android;
+using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Plugin.Media;
+
+namespace ImageMod
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button captureButton;
+        ImageView thisImageView;
+
+        readonly string[] permissionGroup =
+        {
+            Manifest.Permission.ReadExternalStorage,
+            Manifest.Permission.WriteExternalStorage
+        };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            captureButton = (Button)FindViewById(Resource.Id.captureButton);
+            thisImageView = (ImageView)FindViewById(Resource.Id.thisImageView);
+
+            captureButton.Click += CaptureButton_Click;
+            RequestPermissions(permissionGroup, 0);
+        }
+
+        private void CaptureButton_Click(object sender, System.EventArgs e)
+        {
+            UploadPhoto();
+        }
+
+        async void UploadPhoto()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                Toast.MakeText(this, "Upload not supported on this device", ToastLength.Short).Show();
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Full,
+                CompressionQuality = 40
+
+            });
+
+            // Convert file to byre array, to bitmap and set it to our ImageView
+
+            byte[] imageArray = System.IO.File.ReadAllBytes(file.Path);
+            Bitmap bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
+            thisImageView.SetImageBitmap(bitmap);
+        }
+
+        async void CropPhoto()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                Toast.MakeText(this, "Upload not supported on this device", ToastLength.Short).Show();
+                return;
+            }
+
+            var file = await CrossMedia.Current.
+
+            var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Full,
+                CompressionQuality = 40
+
+            });
+
+            // Convert file to byre array, to bitmap and set it to our ImageView
+
+            byte[] imageArray = System.IO.File.ReadAllBytes(file.Path);
+            Bitmap bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
+            thisImageView.SetImageBitmap(bitmap);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
